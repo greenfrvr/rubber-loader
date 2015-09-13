7c30e3909ca6c21db3abac41ed25f0ba7d60ca9e
Artsiom Grintsevich <greenfrvr@gmail.com>
2015-09-13 16:52:44 +0300

    fabricio + fixes

---
+++ sample/src/main/java/com/greenfrvr/rubberloader/sample/App.java
@@ -0,0 +1,21 @@
+package com.greenfrvr.rubberloader.sample;
+
+import android.app.Application;
+
+import com.crashlytics.android.Crashlytics;
+
+import io.fabric.sdk.android.Fabric;
+
+/**
+ * Created by greenfrvr
+ */
+public class App extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        if(!BuildConfig.DEBUG){
+            Fabric.with(this, new Crashlytics());
+        }
+    }
+}
--- sample/src/main/java/com/greenfrvr/rubberloader/sample/MainActivity.java
+++ sample/src/main/java/com/greenfrvr/rubberloader/sample/MainActivity.java
@@ -6,21 +6,18 @@ import android.support.v7.app.AppCompatActivity;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
-import timber.log.Timber;
 
 public class MainActivity extends AppCompatActivity {
 
     protected @Bind(R.id.viewpager) ViewPager viewPager;
-    private SamplesAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Timber.plant(new Timber.DebugTree());
         ButterKnife.bind(this);
 
-        adapter = new SamplesAdapter(getSupportFragmentManager(), this);
+        SamplesAdapter adapter = new SamplesAdapter(getSupportFragmentManager(), this);
         viewPager.setAdapter(adapter);
     }
 
